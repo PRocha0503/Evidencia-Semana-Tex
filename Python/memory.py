a5a7ef0b55ff7c7df8a3fe6f87cc6a1a1766d136
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2021-09-15 14:53:23 -0500

    Restored 8x8 tiles

--- Python/memory.py
+++ Python/memory.py
@@ -3,9 +3,6 @@
 Exercises:
 
 1. Count and print how many taps occur.
-    Using a global variable named taps, we add 1 everytime we press a tile.
-        - Alfredo Jeong Hyun Park, 2021/09/15, 12:23 PM.
-
 2. Decrease the number of tiles to a 4x4 grid.
 3. Detect when all tiles are revealed.
 4. Center single-digit tile.
@@ -15,12 +12,13 @@ Exercises:
 
 from random import *
 from turtle import *
+
 from freegames import path
 
 car = path('car.gif')
-tiles = list(range(8)) * 2
+tiles = list(range(32)) * 2
 state = {'mark': None}
-hide = [True] * 16
+hide = [True] * 64
 taps = 0
 
 def square(x, y):
@@ -35,13 +33,16 @@ def square(x, y):
         left(90)
     end_fill()
 
+
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
+
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+
 
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
@@ -49,7 +50,7 @@ def tap(x, y):
     mark = state['mark']
     global taps
     taps += 1
-    print("Taps: " + str(taps))
+    print("Total taps: " + str(taps))
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
@@ -58,6 +59,7 @@ def tap(x, y):
         hide[mark] = False
         state['mark'] = None
 
+
 def draw():
     "Draw image and tiles."
     clear()
@@ -65,7 +67,7 @@ def draw():
     shape(car)
     stamp()
 
-    for count in range(16):
+    for count in range(64):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
@@ -81,6 +83,7 @@ def draw():
 
     update()
     ontimer(draw, 100)
+
 
 shuffle(tiles)
 setup(420, 420, 370, 0)
